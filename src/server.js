5b240f3cc58434c389bb567999fef6553e4d630a
Vinicius Santos <125936705+vinicius1007@users.noreply.github.com>
2026-04-27 22:18:12 -0300

    criação do endpoint de listagem de usuários

--- src/server.js
+++ src/server.js
@@ -6,9 +6,25 @@ function callbackDaRaiz( request, response ) {
     response.send("Tropa do mantem!")
 }
 
+function callbackListaUsusarios(request, response) {
+const listaUsuarios = [ 
+     {
+        id: 1,
+        name: "Vinicius"
+    
+     }
+ ]
+
+    response.json(listaUsuarios)
+    
+}
+
 app.get("/", callbackDaRaiz)
+
+app.get("/users", callbackListaUsusarios)
 
 app.listen(3000, () => {
     console.log("API rodando em http://localhost:3000")
 })
 
+  
